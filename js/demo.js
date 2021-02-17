de421339c555bc26aa2187b4c4436f85949ccc9c
lauvipas <laura.vidal.pastor@gmail.com>
2021-02-17 11:01:57 +0100

    actualizar img

--- js/demo.js
+++ js/demo.js
@@ -1,3 +1,3 @@
 document.getElementById("name").value = "Laura";
 document.getElementById("lastName").value ="Vidal Pastor";
-document.getElementById("myavatar").src = "images/myavatar.png"
+document.getElementById("myavatar").src = "images/myAvatar.png"
